a15eb13527df61b73560a21ca2d84f972583f5e7
Vova0211 <chernousov_07@bk.ru>
2026-02-24 21:40:48 +0300

    completed

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ const fastify = Fastify({
   logger: true
 });
 
+function validateTask(taskId, data, reply) {
+  const task = tasks.find(e => e.id == taskId)
+  if (!task) {
+    reply.code(404).send({ message: 'Task not found' });
+    return
+  }
+  const dataKeys = Object.keys(data)
+  if (!dataKeys.includes('title') || !dataKeys.includes('completed')) {
+    reply.code(400).send({ message: 'Invalid data' })
+  }
+}
+
+function updateTask(id, data) {
+  const index = tasks.findIndex(e => e.id == id)
+  const task = tasks[index]
+  const updated = tasks.with(index, { ...task, ...data })
+  tasks.splice(0, tasks.length, ...updated)
+}
 const tasks = []
 
 fastify.register(fastifyStatic, {
@@ -24,28 +42,42 @@ fastify.register(fastifyView, {
   propertyName: 'render'
 });
 
+
+
 fastify.get('/', (request, reply) => {
-  reply.render('index.pug', { tasks, title: 'TaskBoard'});
+  reply.status(200).render('index.pug', { tasks });
 });
 
 fastify.post('/add', (request, reply) => {
-  const name = JSON.parse(request.body).data
-  tasks.push({ id: uuidv4(), name })
-  reply.status(201).send()
+  const body = JSON.parse(request.body)
+  const task = { id: uuidv4(), title: body.title, completed: false }
+  tasks.push(task)
+  reply.status(201).send(task)
+});
+
+fastify.put('/update/:id', async (request, reply) => {
+  const taskId = request.params.id
+  const data = JSON.parse(request.body)
+  validateTask(taskId, data, reply)
+  updateTask(taskId, data)
+
+  reply.status(200).send({ message: 'OK' })
 });
 
 fastify.delete('/delete/:id', (request, reply) => {
-  const deleteId = request.params.id
-  const updatedTasks = tasks.filter(e => e.id != deleteId)
+  const taskId = request.params.id
+  const updatedTasks = tasks.filter(e => e.id != taskId)
+  if (updatedTasks.length == tasks.length) reply.status(404).send({ message: 'Item not found' })
   tasks.splice(0, tasks.length, ...updatedTasks)
-  reply.status(204).send()
+  reply.status(200).send({ message: 'OK' })
 });
 
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
-    console.log('Server listening on http://localhost:3000');
+    const settings = { port: 3000 }
+    await fastify.listen(settings);
+    console.log(`Server listening on http://localhost:${settings.port}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 document.querySelector('form').addEventListener('submit', async e => {
     e.preventDefault()
-    const data = e.target.task.value
-    await fetch('/add', {method: 'POST', body: JSON.stringify({data})})
+    const title = e.target.task.value
+    const resp = await fetch('/add', {method: 'POST', body: JSON.stringify({title}) })
+    console.log(await resp.json())
     location.reload()
 })
 document.querySelectorAll('.btn_delete').forEach(btn => {
     btn.addEventListener('click', async e => {
         e.preventDefault()
         const id = e.target.parentNode.id
-        await fetch(`/delete/${id}`, { method: 'DELETE' })
+        const response = await fetch(`/delete/${id}`, { method: 'DELETE' })
+        location.reload()
+    })
+})
+document.querySelectorAll('.btn_update').forEach(btn => {
+    btn.addEventListener('click', async e => {
+        e.preventDefault()
+        const id = e.target.parentNode.id
+        let title = e.target.parentNode.querySelector('.title').value.trim()
+        if (title == '') title = e.target.parentNode.firstChild.textContent
+        const completed = e.target.parentNode.querySelector('.completed').checked
+        const response = await fetch(`/update/${id}`, { method: 'PUT', body: JSON.stringify({ title, completed }) })
         location.reload()
     })
 })
